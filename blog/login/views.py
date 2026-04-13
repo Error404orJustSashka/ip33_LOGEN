a0e0ef8c89ffda94808309a1feb161fe3a4b15d1
Sashylka <sashim_meee@icloud.com>
2026-04-13 13:12:57 +0700

    добавлена вьюшка для создания пользователя

--- blog/login/views.py
+++ blog/login/views.py
@@ -1,3 +1,17 @@
+from django.shortcuts import render, redirect
 from django.shortcuts import render
+from .forms import UserForm
 
 # Create your views here.
+def add_user(request):
+    # получили данные. нужно сохранить юзера в базу
+    if request.method == "POST":
+        # получаем данные из формы
+        user = UserForm(request.POST)
+        if user.is_valid():
+            user.save()
+        return redirect('/users/')
+    # это простой запрос, нужно показать форму
+    else:
+        form = UserForm()
+        return render(request, "add_user.html", {'form': form})
